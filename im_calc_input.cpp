990954338066882bd504300a7b71628b086b452b
bb1950328 <bb1950328@gmail.com>
2021-03-08 19:43:36 +0100

    added input handling, added basic buttons

--- im_calc_input.cpp
+++ im_calc_input.cpp
@@ -5,9 +5,166 @@
 #include "im_calc_input.h"
 #include <cstdio>
 #include <iostream>
+#include <memory>
+#include <map>
 #include "IconsFontAwesome5.h"
 
+#if IMCALCINPUT_PLATFORM==IMCALCINPUT_GLFW
+#include <GLFW/glfw3.h>
+#endif
+
+
 namespace ImCalcInput {
+
+    namespace {
+        void OpenCalculator::focusNextTime() {
+            isFocusNextTime = true;
+        }
+
+        void ImCalcInput::OpenCalculator::draw() {
+            bool open = true;
+            if (isFocusNextTime) {
+                ImGui::SetNextWindowFocus();
+                isFocusNextTime = false;
+            }
+            ImGui::Begin(label, &open, ImGuiWindowFlags_AlwaysAutoResize);
+            auto& io = ImGui::GetIO();
+
+            for (const ImWchar &queueChar : io.InputQueueCharacters) {
+                if (strchr(INPUT_QUEUE_HANDLED_BUTTONS, queueChar)) {
+                    buttonPressed(static_cast<Button>(queueChar));
+                }
+            }
+            io.InputQueueCharacters.resize(0);
+
+            ImGui::Text("%s", currentInputText);
+
+            ImVec2 squareButtonSize = {ImGui::GetStyle().FramePadding.x*2+ImGui::GetFontSize(), ImGui::GetStyle().FramePadding.y*2+ImGui::GetFontSize()};
+            ImVec2 landscapeDoubleButtonSize = {squareButtonSize.x*2+ImGui::GetStyle().ItemSpacing.x, squareButtonSize.y};
+
+            drawButton(Button::NUM_7, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::NUM_8, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::NUM_9, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::PLUS, squareButtonSize);
+
+            drawButton(Button::NUM_4, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::NUM_5, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::NUM_6, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::MINUS, squareButtonSize);
+
+            drawButton(Button::NUM_1, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::NUM_2, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::NUM_3, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::DIVISION, squareButtonSize);
+
+            drawButton(Button::BACKSPACE, squareButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::ENTER, landscapeDoubleButtonSize);
+            ImGui::SameLine();
+            drawButton(Button::MULTIPLICATION, squareButtonSize);
+
+            ImGui::End();
+        }
+
+        ImCalcInput::OpenCalculator::OpenCalculator(const char *label, const double *valuePtr) : label(label), inputValue(valuePtr), inputValueType(ValueType::DOUBLE) {}
+        ImCalcInput::OpenCalculator::OpenCalculator(const char *label, const float *valuePtr) : label(label), inputValue(valuePtr), inputValueType(ValueType::FLOAT) {}
+        ImCalcInput::OpenCalculator::OpenCalculator(const char *label, const int *valuePtr) : label(label), inputValue(valuePtr), inputValueType(ValueType::INTEGER) {}
+
+        void ImCalcInput::OpenCalculator::drawButton(Button button, const ImVec2 &size) {
+            static char simpleLabelStr[2] = {0, 0};
+            const char *labelStr;
+            switch (button) {
+                case Button::ENTER: labelStr = ICON_FA_EQUALS; break;
+                case Button::BACKSPACE: labelStr = ICON_FA_BACKSPACE; break;
+                default: labelStr = simpleLabelStr; simpleLabelStr[0] = static_cast<char>(button); break;
+            }
+            const auto tmp = getPlatformKeyIndex(button);
+            const auto idxA = tmp.first, idxB = tmp.second;
+
+            bool highlightButton = false, clickedButton = false, pressedKey = false;
+
+            if (ImGui::IsWindowFocused()) {
+                if ((idxA!=0 && ImGui::IsKeyPressed(idxA)) || (idxB!=0 && ImGui::IsKeyPressed(idxB))) {
+                    pressedKey = true;
+                    highlightButton = true;
+                } else  if ((idxA!=0 && ImGui::IsKeyDown(idxA)) || (idxB!=0 && ImGui::IsKeyDown(idxB))) {
+                    highlightButton = true;
+                }
+            }
+
+            if (highlightButton) {
+                ImGui::PushStyleColor(ImGuiCol_Button, ImGui::GetStyleColorVec4(ImGuiCol_ButtonActive));
+            }
+            if (ImGui::Button(labelStr, size)) {
+                clickedButton = true;
+            }
+            if (highlightButton) {
+                ImGui::PopStyleColor();
+            }
+
+            if (clickedButton || (pressedKey && strchr(INPUT_QUEUE_HANDLED_BUTTONS, (char)button) == nullptr)) {
+                buttonPressed(button);
+            }
+        }
+
+        void ImCalcInput::OpenCalculator::buttonPressed(Button button) {
+            if (currentInputTextEnd-currentInputText>=CURRENT_INPUT_TEXT_LENGTH) {
+                //todo maybe display a warning
+            } else if (Button::BACKSPACE==button) {
+                --currentInputTextEnd;
+                *currentInputTextEnd = '\0';
+            } else if (Button::ENTER==button) {
+                enterPressed();
+            } else {
+                *currentInputTextEnd = static_cast<char>(button);
+                ++currentInputTextEnd;
+                *currentInputTextEnd = '\0';
+            }
+        }
+
+        void ImCalcInput::OpenCalculator::enterPressed() {
+            std::cout << "TODO calculate " << currentInputText << std::endl;
+            currentInputTextEnd = currentInputText;
+            *currentInputTextEnd = '\0';
+        }
+
+        std::pair<int, int> ImCalcInput::OpenCalculator::getPlatformKeyIndex(Button button) {
+#if IMCALCINPUT_PLATFORM==IMCALCINPUT_GLFW
+            switch (button) {
+                case Button::NUM_0: return {GLFW_KEY_0, GLFW_KEY_KP_0};
+                case Button::NUM_1: return {GLFW_KEY_1, GLFW_KEY_KP_1};
+                case Button::NUM_2: return {GLFW_KEY_2, GLFW_KEY_KP_2};
+                case Button::NUM_3: return {GLFW_KEY_3, GLFW_KEY_KP_3};
+                case Button::NUM_4: return {GLFW_KEY_4, GLFW_KEY_KP_4};
+                case Button::NUM_5: return {GLFW_KEY_5, GLFW_KEY_KP_5};
+                case Button::NUM_6: return {GLFW_KEY_6, GLFW_KEY_KP_6};
+                case Button::NUM_7: return {GLFW_KEY_7, GLFW_KEY_KP_7};
+                case Button::NUM_8: return {GLFW_KEY_8, GLFW_KEY_KP_8};
+                case Button::NUM_9: return {GLFW_KEY_9, GLFW_KEY_KP_9};
+                case Button::BACKSPACE: return {GLFW_KEY_BACKSPACE, 0};
+                case Button::PLUS: return {GLFW_KEY_KP_ADD, 0};
+                case Button::MINUS: return {GLFW_KEY_KP_SUBTRACT, 0};
+                case Button::MULTIPLICATION: return {GLFW_KEY_KP_MULTIPLY, 0};
+                case Button::DIVISION: return {GLFW_KEY_KP_DIVIDE, 0};
+                case Button::ENTER: return {GLFW_KEY_ENTER, GLFW_KEY_KP_ENTER};
+                default: return {0, 0};
+            }
+#else
+#error set IMCALCINPUT_PLATFORM to a supported value
+#endif
+        }
+
+        std::map<void*, std::unique_ptr<OpenCalculator>> openCalculators;
+    }
 
     bool InputDouble(const char *label, double *v, double step, double step_fast, const char *format, ImGuiInputTextFlags flags) {
         const auto childLabelSize = 11 + strlen(label);
@@ -17,7 +174,12 @@ namespace ImCalcInput {
             ImGui::InputDouble("", v, step, step_fast, format, flags);
             ImGui::SameLine();
             if (ImGui::Button(ICON_FA_CALCULATOR)) {
-                std::cout << "TODO open calculator" << std::endl;
+                auto it = openCalculators.find(v);
+                if (it==openCalculators.end()) {
+                    openCalculators[v] = std::make_unique<OpenCalculator>(label, v);
+                } else {
+                    it->second->focusNextTime();
+                }
             }
             ImGui::SameLine();
             ImGui::Text("%s", label);
@@ -25,5 +187,11 @@ namespace ImCalcInput {
             return true;
         }
         return false;
+    }
+
+    void drawOpenCalculators() {
+        for (const auto &calculator : openCalculators) {
+            calculator.second->draw();
+        }
     }
 }
--- im_calc_input.h
+++ im_calc_input.h
@@ -6,8 +6,78 @@
 #define IMCALCINPUTTESTING_IM_CALC_INPUT_H
 
 #include <imgui.h>
+#include <utility>
 
 namespace ImCalcInput {
+
+    namespace {
+        enum class ValueType {
+            INTEGER,
+            FLOAT,
+            DOUBLE,
+        };
+        enum class Button : char {
+            NUM_0 = '0',
+            NUM_1 = '1',
+            NUM_2 = '2',
+            NUM_3 = '3',
+            NUM_4 = '4',
+            NUM_5 = '5',
+            NUM_6 = '6',
+            NUM_7 = '7',
+            NUM_8 = '8',
+            NUM_9 = '9',
+            BACKSPACE = '\b',
+            ROUND_BRACKET_OPEN = '(',
+            ROUND_BRACKET_CLOSE = ')',
+            SQUARE_BRACKET_OPEN = '[',
+            SQUARE_BRACKET_CLOSE = ']',
+            PLUS = '+',
+            MINUS = '-',
+            MULTIPLICATION = '*',
+            DIVISION = '/',
+            ENTER = '\n'
+        };
+
+        const char* INPUT_QUEUE_HANDLED_BUTTONS = "0123456789()[]+-*/";
+
+        class OpenCalculator {
+            const char* label;
+            const void* const inputValue;
+            const ValueType inputValueType;
+            bool isFocusNextTime = false;
+
+            static constexpr int CURRENT_INPUT_TEXT_LENGTH = 256;
+            char currentInputText[CURRENT_INPUT_TEXT_LENGTH] = {0};
+            char* currentInputTextEnd = currentInputText;
+
+            void buttonPressed(Button button);
+            void enterPressed();
+            void drawButton(Button button, const ImVec2 &size);
+            std::pair<int, int> getPlatformKeyIndex(Button button);
+
+        public:
+            void draw();
+            void focusNextTime();
+
+            explicit OpenCalculator(const char *label, const double *valuePtr);
+            explicit OpenCalculator(const char *label, const float *valuePtr);
+            explicit OpenCalculator(const char *label, const int *valuePtr);
+
+        };
+    }
+
     bool InputDouble(const char* label, double* v, double step = 0.0, double step_fast = 0.0, const char* format = "%.6f", ImGuiInputTextFlags flags = 0);
+
+    void drawOpenCalculators();
 }
+
+#define IMCALCINPUT_GLFW 0
+//todo define other libraries
+
+#ifndef IMCALCINPUT_PLATFORM
+//override this variable if you aren't using GLFW
+#define IMCALCINPUT_PLATFORM IMCALCINPUT_GLFW
+#endif
+
 #endif //IMCALCINPUTTESTING_IM_CALC_INPUT_H
